7d7bbec076f1ee4587566efc15f3dbe1823b70df
Dedes73 <federico.lepore73@gmail.com>
2025-12-05 13:43:28 +0100

    ex 8

--- JS Exercises/D5.js
+++ JS Exercises/D5.js
@@ -94,6 +94,14 @@ console.log(justTrims);
     Cicla l'array "cars" e costruisci un if/else statament per mostrare due diversi messaggi in console. Se la prima lettera della proprietà
     "color" ha valore "b", mostra in console "Fizz". Altrimenti, mostra in console "Buzz".
 */
+for (let i = 0; i < cars.length; i++) {
+  let color = cars[i].color.split("");
+  if (color[0] === "b") {
+    console.log("Fizz");
+  } else {
+    console.log("Buzz");
+  }
+}
 
 /* ESERCIZIO 9
     Utilizza un ciclo while per stampare in console i valori del seguente array numerico fino al raggiungimento del numero 32.
